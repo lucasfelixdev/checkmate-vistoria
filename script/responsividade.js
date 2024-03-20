1b078c3093421d859bdd1f53b94d63a9b722295f
Lukas Félix <lucas2009jesus@gmail.com>
2024-03-19 23:43:56 -0300

    UP Responsive

--- script/responsividade.js
+++ script/responsividade.js
@@ -1,12 +1,13 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 10,
+    autoplay: {
+      delay: 4500,
+      disableOnInteraction: false
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
-      autoplay: {
-        delay: 1000,
-      },
     },
     breakpoints: {
       640: {
@@ -28,11 +29,18 @@ var swiper = new Swiper(".mySwiper", {
 let show = true;
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = document.querySelector(".menu-toggle")
+const itens = document.querySelectorAll(".navUlDestk a");
 
 menuToggle.addEventListener("click", () =>{
-
     document.body.style.overflow = show ? "hidden" : "initial"
-
     menuSection.classList.toggle("on", show)
     show = !show;
 })
+
+itens.forEach(item => {
+  item.addEventListener("click", () => {
+    menuSection.classList.remove("on");
+    document.body.style.overflow = "initial";
+    show = true;
+  });
+});
